f910451e1727d0d4689a8233ed9e8cd4199da2cb
Lance Chaney <lance.e.chaney@gmail.com>
2017-08-08 11:55:47 +1200

    Perf improvement

--- VBO/VBO/VBO/vertex_shader.glsl
+++ VBO/VBO/VBO/vertex_shader.glsl
@@ -10,15 +10,16 @@ uniform float currentTime;
 out vec3 outColor;
 
 const float PI = 3.14;
-const float spd = 5.0;
+const float speed = 5.0;
 const float offset = 2.0 * PI / 3.0;
 
 void main() {
     float theta = atan(position.y, position.x);
 
-    outColor.r = (cos(theta + spd * currentTime) + 1.0) / 2.0;
-    outColor.b = (cos(theta + spd * currentTime + offset) + 1.0) / 2.0;
-    outColor.g = (cos(theta + spd * currentTime + 2 * offset) + 1.0) / 2.0;
+    theta += speed * currentTime;
+    outColor.r = (cos(theta) + 1.0) / 2.0;
+    outColor.b = (cos(theta + offset) + 1.0) / 2.0;
+    outColor.g = (cos(theta + 2.0 * offset) + 1.0) / 2.0;
 
     mat4 R = mat4(
         cos(currentTime), sin(currentTime), 0.0, 0.0,
